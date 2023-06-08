260223e5c8c9075bd570ec08f75bca0f215c08ab
Arindam Maity <84143511+arindammaity@users.noreply.github.com>
2023-06-08 12:45:53 +0530

    Add files via upload

---
+++ Solution48.java
@@ -0,0 +1,17 @@
+package ilp;
+import java.util.*;
+public class Solution48
+{
+	public static void main(String[] args) {
+		   String s ="4PREP2INSTAA6";
+		   int sum=0;
+		   for (int i = 0; i < s.length(); i++) {
+		      if(Character.isDigit(s.charAt(i))) 
+		      sum=sum+Character.getNumericValue(s.charAt(i));
+		      }
+		   System.out.println("Sum of all the digit present in String : "+sum);
+		  }
+		}
+
+
+
